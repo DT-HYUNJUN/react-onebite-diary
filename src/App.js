73c96ae8ec10d1d41279fe5b26b9e66c1454e931
Park <angle307@naver.com>
2023-08-12 15:08:45 +0900

    [DELETE] console.log

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function App() {
   }
 
   const onRemove = (targetId) => {
-    console.log(`${targetId}가  삭제되었습니다.`)
     const newDiaryList = data.filter((it) => it.id !== targetId )
     setData(newDiaryList)
   }
